6ee44f030817904237fb7406d4a7cbbfdcea6830
unknown <akramer2016@02510pc.francisparker.org>
2015-03-18 14:49:20 -0700

    switch to Tremaux's algorithm

--- src/Maze.java
+++ src/Maze.java
@@ -3,8 +3,9 @@ import java.util.ArrayList;
 public class Maze
 {
 	public int NUM_ROWS, NUM_COLS;
-	public static final int PATH = 0, WALL = 1, START = 2, END = 3;
+	public static final int PATH = 0, WALL = 1, START = 2, END = 3, MARKED = 4, DONE = 5;
 	public static int[][] mazeArray;
+	
 
 	public Maze(int NUM_ROWS, int NUM_COLS)
 	{
@@ -16,7 +17,9 @@ public class Maze
 	public static void main(String[] args)
 	{
 		new Maze(8, 8);
-
+		addWall(1,1);
+		addWall(0,1);
+		
 		addWall(2, 4);
 		addWall(2, 5);
 		addWall(2, 6);
@@ -25,12 +28,20 @@ public class Maze
 		addWall(4, 2);
 		addWall(4, 3);
 		addWall(4, 4);
+		addWall(4, 1);
+		addWall(4, 0);
+		addWall(3,6);
+		addWall(4,6);
+		addWall(5,6);
+		addWall(6,6);
+		addWall(5,3);
+		addWall(1,6);
 		addWall(5, 2);
 		addWall(6, 2);
 		addWall(7, 2);
 
-		setStart(1, 1);
-		setEnd(6, 7);
+		setStart(0, 0);
+		setEnd(7, 7);
 
 		printMaze();
 	}
@@ -70,5 +81,44 @@ public class Maze
 			}
 			System.out.println();
 		}
+	}
+	public void solve(int[][] mazeArray)
+	{
+		for(int i = 0; i < mazeArray.length; i++)
+		{
+			for(int j = 0; j < mazeArray[0].length; j++)
+			{
+				
+			}
+		}
+	}
+	public void mark(int row, int col)
+	{
+		mazeArray[row][col] = MARKED;
+	}
+	public void done(int row, int col)
+	{
+		mazeArray[row][col] = DONE;
+	}
+	public int getNumNeighbors(int row, int col)
+	{
+		int numNeighbors = 0;
+		if(mazeArray[row-1][col-1] == PATH)
+			numNeighbors++;
+		if(mazeArray[row-1][col] == PATH)
+			numNeighbors++;
+		if(mazeArray[row-1][col+1] == PATH)
+			numNeighbors++;
+		if(mazeArray[row][col-1] == PATH)
+			numNeighbors++;
+		if(mazeArray[row][col+1] == PATH)
+			numNeighbors++;
+		if(mazeArray[row+1][col-1] == PATH)
+			numNeighbors++;
+		if(mazeArray[row+1][col] == PATH)
+			numNeighbors++;
+		if(mazeArray[row+1][col+1] == PATH)
+			numNeighbors++;
+		return numNeighbors;
 	}
 }
